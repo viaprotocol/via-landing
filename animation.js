a85fdd593856f1abc726c5f7c0714f8a44c65b78
iower <iower@tutanota.com>
2022-02-14 12:16:39 +0500

    Animate bridges too

--- animation.js
+++ animation.js
@@ -1,44 +1,51 @@
-var wrapper = document.querySelector('.dexs .items')
+function animate(selector, numberOfItems) {
+  var wrapper = document.querySelector(selector)
+  var itemWidth = 40
 
-var wrapperWidth, gap
+  var wrapperWidth, gap
 
-var numberOfItems = 40
-var itemWidth = 40
+  function reinit() {
+    wrapperWidth = wrapper.clientWidth
+    var slots = Math.floor(wrapperWidth / itemWidth)
+    gap = (wrapperWidth - slots * itemWidth) / (slots - 1)
+  }
 
-function reinit() {
-  wrapperWidth = wrapper.clientWidth
-  var slots = Math.floor(wrapperWidth / itemWidth)
-  gap = (wrapperWidth - slots * itemWidth) / (slots - 1)
-}
+  window.addEventListener('resize', reinit)
+  reinit()
 
-window.addEventListener('resize', reinit)
-reinit()
+  var i = 1
 
-var i = 1
-
-function redraw() {
-  for (let j = 1; j <= numberOfItems; j++) {
-    var pos
-    if (j === numberOfItems + 1 - i) {
-      pos = -120
-    } else {
-      pos = ((i + j - 2) % numberOfItems) * (itemWidth + gap)
+  function redraw() {
+    for (let j = 1; j <= numberOfItems; j++) {
+      var pos
+      if (j === numberOfItems + 1 - i) {
+        pos = -120
+      } else {
+        pos = ((i + j - 2) % numberOfItems) * (itemWidth + gap)
+      }
+      setPosition(j, pos)
     }
-    setPosition(j, pos)
+
+    if (itemWidth * numberOfItems <= wrapperWidth) {
+      return
+    }
+
+    if (i === numberOfItems) {
+      i = 1
+    } else {
+      i++
+    }
   }
 
-  if (i === numberOfItems) {
-    i = 1
-  } else {
-    i++
+  function setPosition(n, pos) {
+    var img = document.querySelector(selector + ' [src*="-' + n + '."]')
+    img.style.opacity = (pos < 0 || pos > wrapperWidth) ? 0 : 1
+    img.style.transform = 'translateX(' + pos + 'px)'
   }
+
+  redraw()
+  setInterval(redraw, 1000)
 }
 
-function setPosition(n, pos) {
-  var img = document.querySelector('[src*="dex-' + n + '."]')
-  img.style.opacity = (pos < 0 || pos > wrapperWidth) ? 0 : 1
-  img.style.transform = 'translateX(' + pos + 'px)'
-}
-
-redraw()
-setInterval(redraw, 1000)
+animate('.dexs .items', 40)
+animate('.bridges .items', 10)
