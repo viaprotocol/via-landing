ce53cfe2940c5b9404b888092c5c14426403eea4
Aleksey Lisun <insuline.ru@gmail.com>
2022-05-27 17:30:24 +0800

    ✨ feat: desktop list products

--- src/js/components/desktop-header.js
+++ src/js/components/desktop-header.js
@@ -2,11 +2,22 @@ export default class DesktopHeader {
   constructor() {
     this._moreSocialsButton = document.querySelector('.header__more-button')
     this._moreSocialEl = document.querySelector('.header__more-socials')
+
+    this._productsButton = document.querySelector('.header__products-button')
+    this._productsEl = document.querySelector('.header__products')
+
+    this.isSocialsOpened = false
+    this.isProductsOpened = false
   }
 
   _toggleSocials() {
-    this._moreSocialsButton.classList.toggle('header__more-button--opened')
-    this._moreSocialEl.classList.toggle('header__more-socials--opened')
+    this._moreSocialsButton.classList.toggle('header__more-button--opened', this.isSocialsOpened)
+    this._moreSocialEl.classList.toggle('header__more-socials--opened', this.isSocialsOpened)
+  }
+
+  _toggleProducts() {
+    this._productsButton.classList.toggle('header__products-button--opened', this.isProductsOpened)
+    this._productsEl.classList.toggle('header__products--opened', this.isProductsOpened)
   }
 
   _initHandleMoreSocialButton() {
@@ -14,11 +25,25 @@ export default class DesktopHeader {
       e.preventDefault()
       e.stopPropagation()
 
+      this.isSocialsOpened = !this.isSocialsOpened
+
       this._toggleSocials()
+    })
+  }
+
+  _initHandleProductsButton() {
+    this._productsButton.addEventListener('click', e => {
+      e.preventDefault()
+      e.stopPropagation()
+
+      this.isProductsOpened = !this.isProductsOpened
+
+      this._toggleProducts()
     })
   }
 
   init() {
     this._initHandleMoreSocialButton()
+    this._initHandleProductsButton()
   }
 }
