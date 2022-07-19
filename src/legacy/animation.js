9a544e4303354fee01bd6f33518b3a20da7d8668
iower <iower@tutanota.com>
2022-07-19 12:54:02 +0500

    Get tools from the backend

--- src/legacy/animation.js
+++ src/legacy/animation.js
@@ -30,7 +30,7 @@ function animate(selector, numberOfItems) {
   }
 
   function setPosition(n, pos) {
-    var img = document.querySelector(selector + ' [src*="-' + n + '."]')
+    var img = document.querySelector(selector + ' img:nth-child(' + n + ')')
     img.style.opacity = pos < 0 || pos >= wrapperWidth ? 0 : 1
     img.style.transform = 'translateX(' + pos + 'px)'
   }
@@ -42,5 +42,28 @@ function animate(selector, numberOfItems) {
   }, 1)
 }
 
-animate('.dexs .items', 40)
-animate('.bridges .items', 11)
+const append = (item, parent) => {
+  const image = document.createElement('img')
+  image.setAttribute('src', item.logoURI)
+  image.setAttribute('height', 40)
+  image.setAttribute('width', 40)
+  image.setAttribute('alt', item.name)
+  parent.appendChild(image)
+}
+
+const dexsContainer = document.querySelector('.dexs .items')
+const bridgesContainer = document.querySelector('.bridges .items')
+
+fetch('https://router-api.via.exchange/api/v2/tools')
+  .then(response => response.json())
+  .then(data => {
+    const dexs = data.tools.filter(tool => tool.type === 'swap')
+    dexs.forEach(dex => { append(dex, dexsContainer) })
+    animate('.dexs .items', dexs.length)
+
+    const bridges = data.tools.filter(tool => tool.type === 'cross')
+    bridges.forEach(bridge => { append(bridge, bridgesContainer) })
+    animate('.bridges .items', bridges.length)
+
+  })
+  .catch(console.error)
