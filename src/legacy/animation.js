44e5b5e18566f3cc9881400070731e153c1421d3
iower <iower@tutanota.com>
2022-08-08 21:40:31 +0500

    Some visual improvements

--- src/legacy/animation.js
+++ src/legacy/animation.js
@@ -2,6 +2,48 @@ var dexsWrapper = document.querySelector('.dexs .items')
 var bridgesWrapper = document.querySelector('.bridges .items')
 var size = 40
 
+function animate(wrapper) {
+  var numberOfItems = wrapper.children.length
+  var wrapperWidth, slots, gap
+
+  function reinit() {
+    wrapperWidth = wrapper.clientWidth
+    slots = Math.min(Math.floor(wrapperWidth / size), window.innerWidth >= 960 ? 10 : 9000)
+    gap = (wrapperWidth - slots * size) / (slots - 1)
+  }
+
+  window.addEventListener('resize', reinit)
+  reinit()
+
+  var pointer = 1
+
+  function redraw() {
+    for (var i = 1; i <= numberOfItems; i++) {
+      var pos = (size + gap) * ((slots - i + pointer - 1) % numberOfItems)
+      pos = pos >= wrapperWidth + size + gap ? -120 : pos
+      setPosition(i, pos)
+    }
+
+    if (size * numberOfItems <= wrapperWidth) {
+      return
+    }
+
+    pointer = pointer === numberOfItems ? 1 : ++pointer
+  }
+
+  function setPosition(n, pos) {
+    var img = wrapper.querySelector('img:nth-child(' + n + ')')
+    img.style.opacity = pos < 0 || pos >= wrapperWidth ? 0 : 1
+    img.style.transform = 'translateX(' + pos + 'px)'
+  }
+
+  redraw()
+  setInterval(redraw, 1000)
+  setTimeout(function () {
+    wrapper.classList.add('inited')
+  }, 1)
+}
+
 function append(item, parent) {
   var image = document.createElement('img')
   image.setAttribute('src', item.logoURI)
@@ -16,8 +58,10 @@ fetch('https://router-api.via.exchange/api/v2/tools')
   .then(function(data) {
     var dexs = data.tools.filter(function(tool) { return tool.type === 'swap' })
     dexs.forEach(function(dex) { append(dex, dexsWrapper) })
+    animate(dexsWrapper)
 
     var bridges = data.tools.filter(function(tool) { return tool.type === 'cross' })
     bridges.forEach(function(bridge) { append(bridge, bridgesWrapper) })
+    animate(bridgesWrapper)
   })
   .catch(console.error)
