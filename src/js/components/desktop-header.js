ba291e6ad32f67d97ad75a10ce0bdfd358d65c9f
iower <iower@tutanota.com>
2022-08-09 17:52:59 +0500

    Header: remove old socials toggle

--- src/js/components/desktop-header.js
+++ src/js/components/desktop-header.js
@@ -1,36 +1,13 @@
 export default class DesktopHeader {
   constructor() {
-    this._moreSocialsButton = document.querySelector('.header__more-button')
-    this._moreSocialEl = document.querySelector('.header__more-socials')
-    this._socialEl = document.querySelector('.header__socials')
-
     this._productsButton = document.querySelector('.header__products-button')
     this._productsEl = document.querySelector('.header__products')
-
-    this.isSocialsOpened = false
     this.isProductsOpened = false
-  }
-
-  _toggleSocials() {
-    this._moreSocialsButton.classList.toggle('header__more-button--opened', this.isSocialsOpened)
-    this._moreSocialEl.classList.toggle('header__socials--opened', this.isSocialsOpened)
-    this._socialEl.classList.toggle('header__socials--opened', this.isSocialsOpened)
   }
 
   _toggleProducts() {
     this._productsButton.classList.toggle('header__products-button--opened', this.isProductsOpened)
     this._productsEl.classList.toggle('header__products--opened', this.isProductsOpened)
-  }
-
-  _initHandleMoreSocialButton() {
-    this._moreSocialsButton.addEventListener('click', e => {
-      e.preventDefault()
-      e.stopPropagation()
-
-      this.isSocialsOpened = !this.isSocialsOpened
-
-      this._toggleSocials()
-    })
   }
 
   _initHandleProductsButton() {
@@ -45,7 +22,6 @@ export default class DesktopHeader {
   }
 
   init() {
-    this._initHandleMoreSocialButton()
     this._initHandleProductsButton()
   }
 }
