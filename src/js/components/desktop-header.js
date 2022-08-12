466a630451366d2933a67bcf50ae4a855c7e0ab5
iower <iower@tutanota.com>
2022-08-12 12:20:11 +0500

    Close menu on outside click

--- src/js/components/desktop-header.js
+++ src/js/components/desktop-header.js
@@ -6,6 +6,7 @@ export default class DesktopHeader {
   }
 
   _toggleProducts() {
+    this.isProductsOpened = !this.isProductsOpened
     this._productsButton.classList.toggle('header__products-button--opened', this.isProductsOpened)
     this._productsEl.classList.toggle('header__products--opened', this.isProductsOpened)
   }
@@ -15,9 +16,21 @@ export default class DesktopHeader {
       e.preventDefault()
       e.stopPropagation()
 
-      this.isProductsOpened = !this.isProductsOpened
-
       this._toggleProducts()
+    })
+
+    window.document.addEventListener('click', e => {
+      let element = e.target
+      do {
+        if (element === this._productsEl) {
+          return
+        }
+        element = element.parentNode
+      } while (element && element !== window.document)
+
+      if (this.isProductsOpened) {
+        this._toggleProducts()
+      }
     })
   }
 
