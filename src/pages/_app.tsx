bc3b2e7f6432e1f5e6444e7a0e444d252413f170
iower <iower@tutanota.com>
2022-08-23 16:46:04 +0500

    Add Tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+
+import 'tailwindcss/tailwind.css'
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
