7ee2e18b336581c244ae5c5e6912a5eae333b47f
iower <iower@tutanota.com>
2022-08-24 16:42:13 +0500

    Fix wrappers

--- src/views/IndexPage/Investors/Investors.tsx
+++ src/views/IndexPage/Investors/Investors.tsx
@@ -14,23 +14,25 @@ function Investors() {
 
   return (
     <Section>
-      <div className="flex items-center mb-4 md:mb-10">
-        <h2 className="">Investors</h2>
-        {!isMobile && becomeLink}
+      <div className="px-1.5 md:px-0">
+        <div className="flex items-center mb-4 md:mb-10">
+          <h2 className="">Investors</h2>
+          {!isMobile && becomeLink}
+        </div>
+        <div className="flex align-bottom justify-start md:justify-evenly gap-2.5">
+          {investors.map(investor => (
+            <div className="p-5 md:p-0 rounded-lg bg-white/5 md:bg-transparent" key={investor.title}>
+              <img
+                className="md:opacity-50"
+                src={`/images/investors/${investor.fileName}`}
+                title={investor.title}
+                alt={investor.title}
+              />
+            </div>
+          ))}
+        </div>
+        {isMobile && becomeLink}
       </div>
-      <div className="flex align-bottom justify-start md:justify-evenly gap-2.5">
-        {investors.map(investor => (
-          <div className="p-5 md:p-0 rounded-lg bg-white/5 md:bg-transparent" key={investor.title}>
-            <img
-              className="md:opacity-50"
-              src={`/images/investors/${investor.fileName}`}
-              title={investor.title}
-              alt={investor.title}
-            />
-          </div>
-        ))}
-      </div>
-      {isMobile && becomeLink}
     </Section>
   )
 }
