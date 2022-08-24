3c885b7fd32203d4d2da045c1e52604c93e0973e
iower <iower@tutanota.com>
2022-08-24 16:15:57 +0500

    Investors: improve layout

--- src/data/investors.ts
+++ src/data/investors.ts
@@ -4,19 +4,7 @@ export const investors = [
     fileName: 'ravicant-capital.svg',
   },
   {
-    title: 'A16z crypto',
-    fileName: 'a16z-crypto.svg',
-  },
-  {
     title: 'Shima Capital',
     fileName: 'shima-capital.svg',
-  },
-  {
-    title: 'Paradigm',
-    fileName: 'paradigm.svg',
-  },
-  {
-    title: 'Multicoin Capital',
-    fileName: 'multicoin-capital.svg',
   },
 ]
--- src/views/IndexPage/Investors/Investors.tsx
+++ src/views/IndexPage/Investors/Investors.tsx
@@ -2,18 +2,33 @@ import React from 'react'
 import { investors } from '@/data/investors'
 import { Section } from '@/components/layout'
 
+const isMobile = true
+
+const becomeLink = (
+  <a className="block mt-6 md:mt-1 -ml-2 md:ml-12 px-2 py-1.5 font-semibold text-white/80 hover:text-white" href="mailto:serafim@via.exchange">Become an Investor</a>
+) 
+
 function Investors() {
   return (
     <Section>
-      <div className="flex items-center mb-10">
+      <div className="flex items-center mb-4 md:mb-10">
         <h2 className="">Investors</h2>
-        <a className="ml-12 px-2 py-1.5 text-white/80 hover:text-white" href="mailto:serafim@via.exchange">Become an Investor</a>
+        {!isMobile && becomeLink}
       </div>
-      <div className="flex align-bottom justify-between">
+      <div className="flex align-bottom justify-start md:justify-evenly gap-2.5">
         {investors.map(investor => (
-          <img src={`/images/investors/${investor.fileName}`} title={investor.title} alt={investor.title} key={investor.title} />
+          <div className="p-5 md:p-0 rounded-lg bg-white/5 md:bg-transparent">
+            <img
+              className="md:opacity-50"
+              src={`/images/investors/${investor.fileName}`}
+              title={investor.title}
+              alt={investor.title}
+              key={investor.title}
+            />
+          </div>
         ))}
       </div>
+      {isMobile && becomeLink}
     </Section>
   )
 }
