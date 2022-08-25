a44730a68a925e0049c7957c26a3d7e3e76b10df
iower <iower@tutanota.com>
2022-08-25 20:56:23 +0500

    Fix Demo section placement

--- src/components/layout/Section/Section.tsx
+++ src/components/layout/Section/Section.tsx
@@ -3,7 +3,7 @@ import cx from 'classnames'
 
 function Section({ className, children }: PropsWithChildren<{className?: string}>) {
   return (
-    <section className={cx(className, 'max-w-[1172px] ml-auto mr-auto px-2.5 md:px-8')}>
+    <section className={cx(className, 'max-w-[1172px] ml-auto mr-auto px-2.5 md:px-8 xl:px-0')}>
       {children}
     </section>
   )
--- src/views/IndexPage/Demo/Demo.tsx
+++ src/views/IndexPage/Demo/Demo.tsx
@@ -1,16 +1,19 @@
+import { Section } from '@/components/layout'
 import { Routes } from './components/Routes'
 import styles from './Demo.module.scss'
 
 function Demo() {
   return (
-    <section className={styles.demo}>
-      <h2 className={styles.title}>All cross-chain<br />routes in one interface</h2>
-      <p className={styles.description}>
-        <span>Be sure that you will make a<br/>cross-chain transfer in the<br/>most efficient and safe way</span>
-        <a href="https://router.via.exchange/" target="_blank" rel="noopener norefferer noreferrer" className={styles.button}>Open</a>
-      </p>
-      <Routes className={styles.routes} />
-    </section>
+    <Section>
+      <div className={styles.demo}>
+        <h2 className={styles.title}>All cross-chain<br />routes in one interface</h2>
+        <p className={styles.description}>
+          <span>Be sure that you will make a<br/>cross-chain transfer in the<br/>most efficient and safe way</span>
+          <a href="https://router.via.exchange/" target="_blank" rel="noopener norefferer noreferrer" className={styles.button}>Open</a>
+        </p>
+        <Routes className={styles.routes} />
+      </div>
+    </Section>
   )
 }
 
