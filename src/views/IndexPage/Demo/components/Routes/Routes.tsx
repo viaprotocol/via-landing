b5eec38c81052decb6da42f2e3d2e9f44b70b422
iower <iower@tutanota.com>
2022-08-30 11:39:56 +0500

    Routes demo: real toAmount

--- src/views/IndexPage/Demo/components/Routes/Routes.tsx
+++ src/views/IndexPage/Demo/components/Routes/Routes.tsx
@@ -9,17 +9,24 @@ import EthLogo from 'public/images/tokens/eth.svg'
 import { Icon } from '@/components/kit'
 import { useRoutes } from '@/hooks/useRoutes/useRoutes'
 import styles from '../../Demo.module.scss'
+import { CURRENCY_CRYPTO, formatValue } from '@/format-crypto/format'
+import { fromDecimal } from './utils'
 
 const ROUTES_ACTIVE_BY_DEFAULT = 3
 
 const Routes: FC<PropsWithChildren<TRoutesProps>> = memo(({ className }) => {
   const routes: TRangingRoute[] = useRoutes()
-  // const routes = []
 
   const isSkeletonsVisible = routes.length < 5
 
   const countOfSkeletons = Math.max(11 - routes.length, 1)
   const skeletonArray = useMemo(() => new Array(countOfSkeletons).fill(null), [countOfSkeletons])
+
+  const toAmount = routes.length
+    ? formatValue(CURRENCY_CRYPTO, fromDecimal(
+      routes[0].toTokenAmount,
+      routes[0].calculatedSteps[routes[0].calculatedSteps.length - 1].toToken.decimals
+    )) : 1
 
   return (
     <section className={className}>
@@ -35,7 +42,7 @@ const Routes: FC<PropsWithChildren<TRoutesProps>> = memo(({ className }) => {
         <div className="flex">
           <EthLogo className="mr-[10px] h-10 w-10" />
           <div className="flex flex-col">
-            <p className="font-semibold text-[18px] tracking-tight">1</p>
+            <p className="font-semibold text-[18px] tracking-tight">{toAmount}</p>
             <span className="text-xs text-white/40">on Arbitrum</span>
           </div>
         </div>
