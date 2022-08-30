89cc88cbd142ba2fa5abe3a5d548914bb745d3ce
iower <iower@tutanota.com>
2022-08-30 19:03:14 +0500

    Restore effect

--- src/components/kit/Tile/Tile.tsx
+++ src/components/kit/Tile/Tile.tsx
@@ -1,5 +1,6 @@
 import cx from 'classnames'
 import type { PropsWithChildren } from 'react'
+import { useState } from 'react'
 
 import type { TTileGroupProps, TTileProps } from './types'
 
@@ -7,28 +8,34 @@ function Tile({ slots = 1, className, icon, title, description, isMobileColumned
   const isOnlyTitle = title && !description
   const isIconAndText = (title || description) && icon
 
-  /*
-    item.onmousemove = function(e) {
-     item.style.setProperty('--cursor-x', e.layerX)
-      item.style.setProperty('--cursor-y', e.layerY)
-    }
-  */
+  const [cursorX, setCursorX] = useState<null | number>(null)
+  const [cursorY, setCursorY] = useState<null | number>(null)
+
+  const setCursorPosition = (e: any) => {
+    setCursorX(e.nativeEvent.layerX)
+    setCursorY(e.nativeEvent.layerY)
+  }
 
   return (
-    <div className={cx(
-      className,
-      'flex col-span-1 min-h-[180px] lg:min-h-[320px] bg-white/5 rounded-2xl overflow-hidden',
-      isMobileColumned ? 'flex-row-reverse lg:flex-col justify-between items-center lg:items-start' : 'flex-col',
-      (isOnlyTitle || slots === 2) && 'lg:items-center lg:justify-center',
-      isIconAndText && 'lg:justify-between',
-      isIconAndText && slots === 2 && 'lg:flex-row-reverse',
-      isMobileColumned ? 'px-5 py-6 lg:px-10 lg:py-10' : 'px-10 py-10',
-      ({
-        1: 'lg:col-span-1',
-        2: 'lg:col-span-2',
-        3: 'lg:col-span-3'
-      }[slots])
-    )}>
+    <div
+      className={cx(
+        'Tile',
+        className,
+        'flex col-span-1 min-h-[180px] lg:min-h-[320px] bg-white/5 rounded-2xl overflow-hidden',
+        isMobileColumned ? 'flex-row-reverse lg:flex-col justify-between items-center lg:items-start' : 'flex-col',
+        (isOnlyTitle || slots === 2) && 'lg:items-center lg:justify-center',
+        isIconAndText && 'lg:justify-between',
+        isIconAndText && slots === 2 && 'lg:flex-row-reverse',
+        isMobileColumned ? 'px-5 py-6 lg:px-10 lg:py-10' : 'px-10 py-10',
+        ({
+          1: 'lg:col-span-1',
+          2: 'lg:col-span-2',
+          3: 'lg:col-span-3'
+        }[slots])
+      )}
+      onMouseMove={setCursorPosition}
+      style={{ '--cursor-x': cursorX, '--cursor-y': cursorY } as React.CSSProperties}
+    >
       {icon &&
         <div>
           {icon}
