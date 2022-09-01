8cba37e4e6efe9a1ac0bbc7c2dd2652c0933dc68
iower <iower@tutanota.com>
2022-09-01 13:34:12 +0500

    Init ForDevelopers block

--- src/views/IndexPage/ForDevelopers/ForDevelopers.tsx
+++ src/views/IndexPage/ForDevelopers/ForDevelopers.tsx
@@ -2,35 +2,42 @@ import React from 'react'
 import { Section } from '@/components/layout'
 import { Tile } from '@/components/kit'
 
-import { GrantProgram, SafeMode, Sdk, Web3Wallets } from './components/'
-
+import GrantProgramIcon from 'public/images/features/grant-program.svg'
 import TrustedTokensIcon from 'public/images/features/trusted-tokens.svg'
 
 function ForDevelopers() {
   return (
     <Section>
       <>
-        <div className="ml-2.5 mb-10 lg:ml-0">
-          <h2 className="mb-3 text-[36px] font-semibold leading-[44px] lg:mb-6 lg:text-[48px] lg:leading-[56px]">For developers</h2>
-          <p className="max-w-[940px] text-[24px] leading-[32px] text-white/40 lg:text-[32px] lg:leading-[40px]">The most potent SDK with all aggregators and security features</p>
-        </div>
+        <div className="ml-2.5 lg:ml-0 mb-10">
+          <h2 className="mb-3 lg:mb-6 font-semibold text-[36px] lg:text-[48px] leading-[44px] lg:leading-[56px]">For developers</h2>
+          <p className="max-w-[940px] text-white/40 text-[24px] lg:text-[32px] leading-[32px] lg:leading-[40px]">The most potent SDK with all aggregators and security features</p>
+          </div>
         <Tile.Group>
-          <Tile slots={3} disableEffect>
-            <Sdk />
-          </Tile>
-          <Tile slots={3} className="lg:min-h-[280px]">
-            <SafeMode />
-          </Tile>
-          <Tile ySlots={2}>
-            <GrantProgram />
-          </Tile>
-          <Tile slots={2}>
-            <Web3Wallets />
-          </Tile>
+          <Tile
+            slots={3}
+            title="SDK"
+            description="Show routes 5 times faster than with other aggregators API"
+          />
+          <Tile
+            slots={3}
+            title="Safe mode"
+            description="Don't worry about hacks and bridge bugs. Your users will not encounter them"
+          />
+          <Tile
+            icon={<GrantProgramIcon/>}
+            title="Grant program"
+            description="Grant program for wallets & multichain dapps"
+          />
+          <Tile
+            slots={2}
+            title="Web3 Wallets library"
+            description="Wallets for 6 ecosystems with multiconnect"
+          />
           <Tile
             slots={2}
             icon={<TrustedTokensIcon/>}
-            title="Multichain trusted token standard"
+            title="Multichian trusted token standart"
             description="Tokenlist of trusted tokens across 25 chains"
             isDesktopReversed
           />
