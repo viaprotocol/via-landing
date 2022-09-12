6dc743369651ddb52954a86e2610579891095066
Aleksey Lisun <insuline.ru@gmail.com>
2022-09-12 12:26:16 +0300

    ✨ feat: fade-in + paddings

--- src/views/IndexPage/Wallets/components/WalletList.tsx
+++ src/views/IndexPage/Wallets/components/WalletList.tsx
@@ -1,16 +1,19 @@
+import { useMedia } from '@/hooks'
 import type { TWalletListProps } from '@/views/IndexPage/Wallets/components/types'
 import Image from 'next/image'
 import { memo } from 'react'
 import Marquee from 'react-fast-marquee'
+import styles from '../Wallets.module.scss'
 
 const WalletList = memo(({ items, delay = 0, speed = 20, direction = 'right' }: TWalletListProps) => {
+  const { isMobile } = useMedia()
   return (
-    <Marquee gradient={false} delay={delay} speed={speed} direction={direction}>
-      <div className="mb-7 flex items-center md:mb-9">
+    <Marquee gradient={false} delay={delay} speed={speed} direction={direction} className={styles.marquee}>
+      <div className="mb-7 flex items-center md:mb-10">
         {items.map((wallet) => {
           return (
-            <div className="mx-3.5 md:mx-5" key={wallet.name}>
-              <Image src={wallet.logo} alt={wallet.name} width={86} height={86} className="md:h-40 md:w-40" />
+            <div className="mx-3.5 h-[86px] w-[86px] md:mx-5 md:h-[120px] md:w-[120px]" key={wallet.name}>
+              <Image src={wallet.logo} alt={wallet.name} width={isMobile ? 86 : 120} height={isMobile ? 86 : 120} />
             </div>
           )
         })}
