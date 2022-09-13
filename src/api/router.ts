9fe97e99f59f65bd808f87d18023e260b674d479
Aleksey Lisun <insuline.ru@gmail.com>
2022-09-13 18:24:54 +0300

    🔧 chore: change api key

--- src/api/router.ts
+++ src/api/router.ts
@@ -1,6 +1,6 @@
 import { Via } from '@viaprotocol/router-sdk'
 
-const DEFAULT_API_KEY = 'e3db93a3-ae1c-41e5-8229-b8c1ecef5583'
+const DEFAULT_API_KEY = 'e3db93a3-ae1c-41e5-8229-b8c1ecef5683'
 export const cli = new Via({ apiKey: DEFAULT_API_KEY, url: 'https://router-api.via.exchange', timeout: 30000 })
 
 // const getLandingRoutes = async (): Promise<any> => {
