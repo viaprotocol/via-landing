3c8d3c594339f9741854515b9e537a7b2384a5c0
iower <iower@tutanota.com>
2022-09-19 17:26:34 +0500

    Remove tg

--- src/components/layout/MobileMenu/MobileMenu.tsx
+++ src/components/layout/MobileMenu/MobileMenu.tsx
@@ -69,12 +69,6 @@ function MobileMenu() {
           </a>
         </li>
         <li>
-          <a className="mobile-menu__social-link" href="https://t.me/viaexchange" target="_blank" rel="noopener noreferrer">
-            <Icon icon="telegram" width={24} height={24} />
-            <span className="visually-hidden">Telegram</span>
-          </a>
-        </li>
-        <li>
           <a className="mobile-menu__social-link" href="https://medium.com/via-exchange" target="_blank" rel="noopener noreferrer">
             <Icon icon="medium" width={24} height={24} />
             <span className="visually-hidden">Medium</span>
