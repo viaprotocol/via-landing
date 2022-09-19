7ce520aebb4669319a421276b28ce03ed5ef4e34
iower <iower@tutanota.com>
2022-09-19 12:27:24 +0500

    Change Footer layout

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -3,11 +3,13 @@ import styles from './Footer.module.scss'
 function Footer() {
   return (
     <div className={styles.Footer}>
-      <img src="images/logo-footer.svg" alt="Via" width="74" height="16" />
-      <div className={styles.descriptionAndYear}>
-        <span className="description">Cross&#8209;chain aggregation protocol, 2022</span>
+      <div className="flex flex-col items-center gap-2.5 lg:flex-row lg:gap-8">
+        <img src="images/logo-footer.svg" alt="Via" width="74" height="16" />
+        <div className={styles.descriptionAndYear}>
+          Cross&#8209;chain aggregation protocol, 2022
+        </div>
       </div>
-      <a href="https://docs.via.exchange/product-docs/" className={styles.docsLink} target="_blank" rel="noreferrer">Docs</a>
+      <div>Socials...</div>
     </div>
   )
 }
