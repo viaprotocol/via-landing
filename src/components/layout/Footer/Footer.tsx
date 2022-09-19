5360101303e6b12c54904b89d9a8e26f1707ef4b
iower <iower@tutanota.com>
2022-09-19 12:50:54 +0500

    Footer: Product hunt

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,15 +1,18 @@
-import styles from './Footer.module.scss'
-
 function Footer() {
   return (
-    <div className={styles.Footer}>
-      <div className="flex flex-col items-center gap-2.5 lg:flex-row lg:gap-8">
-        <img src="images/logo-footer.svg" alt="Via" width="74" height="16" />
-        <div className={styles.descriptionAndYear}>
-          Cross&#8209;chain aggregation protocol, 2022
+    <div className="mt-9 px-6 py-10 text-white/25 lg:mt-[195px] lg:px-[60px]">
+      <div className="flex flex-col items-center lg:flex-row lg:justify-between">
+        <div className="flex flex-col items-center gap-2.5 lg:flex-row lg:gap-8">
+          <img src="images/logo-footer.svg" alt="Via" width="74" height="16" />
+          <div>
+            Cross&#8209;chain aggregation protocol, 2022
+          </div>
         </div>
+        <div>Socials...</div>
       </div>
-      <div>Socials...</div>
+      <div className="mt-[44px] hidden lg:block">
+        <img src="images/producthunt.png" alt="PRODUCT HUNT #1 Product of the Day" width="250" height="54" />
+      </div>
     </div>
   )
 }
