de7903bade73aa90c6d6cefacbbeb1758611e272
iower <iower@tutanota.com>
2022-10-06 19:30:46 +0500

    Header: add 'Launch', remove 'Products' desktop menu

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,18 +1,10 @@
-import { useCallback, useContext, useState } from 'react'
-import cx from 'classnames'
+import { useContext } from 'react'
 
 import styles from './Header.module.scss'
 import { socials } from '@/data/socials'
 import { StateContext } from '@/state'
-import { Icon } from '@/components/kit'
 
 function Header() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
-
-  const toggleMenu = useCallback(() => {
-    setIsMenuOpen(state => !state)
-  }, [])
-
   const { openMobileMenu } = useContext(StateContext)
 
   return (
@@ -40,50 +32,16 @@ function Header() {
         <img src="/images/icons/menu-icon.svg" alt="Menu icon" width="24" height="24" />
       </button>
 
-      <nav className={styles.headerActions}>
-        <a href="https://docs.via.exchange/product-docs/" className={styles.headerActionButton} target="_blank" rel="noreferrer">
+      <nav className={styles.headerButtons}>
+        <a href="https://docs.via.exchange/product-docs" className={styles.headerButton} target="_blank" rel="noreferrer">
           Docs
         </a>
-        <button onClick={toggleMenu} className={cx(
-          styles.headerActionButton,
-          styles.headerProductsButton,
-          isMenuOpen && styles.headerProductsButtonOpened
-        )}>
-          {isMenuOpen ?
-            <Icon icon="close" className="mt-1" />
-            :
-            <span>Products</span>
-          }
-        </button>
-        <div className={cx(
-          styles.headerProducts,
-          isMenuOpen && styles.headerProductsOpened
-        )}>
-          <ul className={styles.headerProductsList}>
-            <li className={styles.menuProduct}>
-              <a href="https://router.via.exchange/" className={styles.menuProductWrapper} target="_blank" rel="noopener noreferrer">
-                <div className={styles.menuProductImageWrapper}>
-                  <img className={styles.menuProductImage} width="24" height="24" src="/images/icons/router.svg" alt="Cross-chain Aggregation Protocol" />
-                </div>
-                <div className={styles.menuProductInfo}>
-                  <h4 className={styles.menuProductTitle}>Cross-chain Aggregation Protocol</h4>
-                  <p className={styles.menuProductDescription}>The best router for any-to-any cross-chain swaps</p>
-                </div>
-              </a>
-            </li>
-            <li className={styles.menuProduct}>
-              <a href="https://github.com/viaprotocol/via-sdk-js" className={styles.menuProductWrapper} target="_blank" rel="noopener noreferrer">
-                <div className={styles.menuProductImageWrapper}>
-                  <img className={styles.menuProductImage} width="24" height="24" src="/images/icons/sdk.svg" alt="SDK" />
-                </div>
-                <div className={styles.menuProductInfo}>
-                  <h4 className={styles.menuProductTitle}>SDK</h4>
-                  <p className={styles.menuProductDescription}>Access to multi-chain for wallets, games and marketplaces</p>
-                </div>
-              </a>
-            </li>
-          </ul>
-        </div>
+        <a href="https://github.com/viaprotocol/via-sdk-js" className={styles.headerButton} target="_blank" rel="noreferrer">
+          SDK
+        </a>
+        <a href="https://router.via.exchange" className={styles.headerButton} target="_blank" rel="noreferrer">
+          Launch
+        </a>
       </nav>
     </header>
   )
