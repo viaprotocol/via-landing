2ea9c19cff993f0b9ed02e7c0b52d16847a5517c
iower <iower@tutanota.com>
2023-04-27 23:08:13 +0500

    Add 'Stats' block

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next'
-import { Demo, Intro, Investors, SafetyFirst, Features, ForDevelopers, TransactionTracking } from '@/views/IndexPage'
+import { Demo, Intro, Stats, Investors, SafetyFirst, Features, ForDevelopers, TransactionTracking } from '@/views/IndexPage'
 
 import { Footer, Header, MobileMenu } from '@/components/layout'
 import { Wallets } from '@/views/IndexPage/Wallets'
@@ -16,23 +16,26 @@ const Home: NextPage = () => {
 
           <main>
             <Intro />
+            <div className="h-[80px] md:h-[100px]" />
+            <Stats />
+            <div className="h-[120px] md:h-[200px]" />
             <Investors />
-            <div className="h-[86px] md:h-[160px]"></div>
+            <div className="h-[86px] md:h-[160px]" />
             <Demo />
-            <div className="h-[120px] md:h-[130px]"></div>
+            <div className="h-[120px] md:h-[130px]" />
             <SafetyFirst />
-            <div className="h-[120px] md:h-[200px]"></div>
+            <div className="h-[120px] md:h-[200px]" />
             <Supported />
             <Features />
-            <div className="h-[120px] md:h-[200px]"></div>
+            <div className="h-[120px] md:h-[200px]" />
             <Ecosystem />
-            <div className="h-[120px] md:h-[200px]"></div>
+            <div className="h-[120px] md:h-[200px]" />
             <div className={styles.leftDot}>
               <Wallets />
             </div>
-            <div className="h-[120px] md:h-[200px]"></div>
+            <div className="h-[120px] md:h-[200px]" />
             <TransactionTracking />
-            <div className="h-[120px] md:h-[200px]"></div>
+            <div className="h-[120px] md:h-[200px]" />
             <ForDevelopers />
           </main>
 
